bfde1934d18d82986f9eb9fda47b3ba9df74dbc0
Wins Julien <vieuxnorris@gmail.com>
2023-02-01 16:52:36 +0100

    Update Leap_year.py

--- Day_3/Leap_year.py
+++ Day_3/Leap_year.py
@@ -4,13 +4,13 @@ year = int(input("Which year do you want to check? "))
 
 #Write your code below this line 👇
 
-
-
 if year % 4 == 0:
-    print("Leap year.")
-elif year % 100 == 0:
-    print("Not leap year.")
-elif year % 400 == 0:
-    print("Leap year.")
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
 else:
     print("Not leap year.")
