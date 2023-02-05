d627f8f75a0d8b367df58cac8cdd36ee9590a247
Wins Julien <vieuxnorris@gmail.com>
2023-02-05 01:20:05 +0100

    Add files via upload

---
+++ Day_14/day-14-Higher-lower.py
@@ -0,0 +1,52 @@
+from replit import clear
+from art import logo, vs
+from game_data import data
+import random
+
+# Global
+SCORE = 0
+
+# Functions
+def game():
+    endGame = False
+    while endGame != True:
+        clear()
+        print(logo)
+        endGame = displayChoices()
+    clear()
+    print(logo)
+    print(f"Sorry, that's wrong. Final score: {SCORE}")
+
+def displayChoices():
+    global SCORE
+    choiceA = random.choice(data)
+    choiceB = random.choice(data)
+
+    while choiceB == choiceA:
+        choiceB = random.choice(data)
+
+    print(f"Compare A: {choiceA['name']}, a {choiceA['description']}, from {choiceA['country']}.")
+    print(vs)
+    print(f"Compare B: {choiceB['name']}, a {choiceB['description']}, from {choiceB['country']}.")
+
+    userChoice = input("Who has more followers? Type 'A' or 'B': ").lower()
+    isRight = checkUserChoice(userChoice, choiceA['follower_count'], choiceB['follower_count'])
+    if isRight:
+        SCORE += 1
+    else:
+        return True
+
+def checkUserChoice(userChoice, followA, followB):
+    if userChoice == 'a':
+        if followA > followB:
+            return True
+        else:
+            return False
+    else:
+        if followB > followA:
+            return True
+        else:
+            return False
+
+# Game
+game()
