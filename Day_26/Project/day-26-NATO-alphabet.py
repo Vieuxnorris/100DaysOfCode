98d52c4859bf12f8e74b4bddab27ab7e33beaa7d
unknown <Vieuxnorris@gmail.com>
2023-02-14 21:06:15 +0100

    modif final day project 26

--- Day_26/Project/day-26-NATO-alphabet.py
+++ Day_26/Project/day-26-NATO-alphabet.py
@@ -1,9 +1,14 @@
 import pandas as pd
 
+nato = []
+
 natoPhoneticDataFrame = pd.read_csv("nato_phonetic_alphabet.csv")
 studentNormalisationIndex = {value.letter: value.code for (index, value) in natoPhoneticDataFrame.iterrows()}
 
-userInput = input(f"Enter a word: ")
-userInputList = [letter.upper() for letter in userInput if letter.isalpha()]
-nato = [phoneticWord for (letter, phoneticWord) in studentNormalisationIndex.items() if letter in userInputList]
+userInput = input(f"Enter a word: ").upper()
+userInputList = [letter for letter in userInput]
+print(userInputList)
+for letter in userInputList:
+    nato += [word for (natoLetters, word) in studentNormalisationIndex.items() if natoLetters == letter]
+
 print(" - ".join(word for word in nato))
