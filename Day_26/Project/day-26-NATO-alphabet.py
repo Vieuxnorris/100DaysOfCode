446a72db51405fe9adbe88fbe87f055e629a198a
unknown <Vieuxnorris@gmail.com>
2023-02-14 21:09:59 +0100

    update security input user

--- Day_26/Project/day-26-NATO-alphabet.py
+++ Day_26/Project/day-26-NATO-alphabet.py
@@ -5,9 +5,9 @@ nato = []
 natoPhoneticDataFrame = pd.read_csv("nato_phonetic_alphabet.csv")
 studentNormalisationIndex = {value.letter: value.code for (index, value) in natoPhoneticDataFrame.iterrows()}
 
-userInput = input(f"Enter a word: ").upper()
-userInputList = [letter for letter in userInput]
-print(userInputList)
+userInput = input(f"Enter a word: ")
+userInputList = [letter.upper() for letter in userInput if letter.isalpha()]
+
 for letter in userInputList:
     nato += [word for (natoLetters, word) in studentNormalisationIndex.items() if natoLetters == letter]
 
