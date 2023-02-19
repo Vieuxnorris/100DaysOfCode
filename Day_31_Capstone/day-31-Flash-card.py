d55c758e1b9839c881f467026e548ef576e10d7a
unknown <Vieuxnorris@gmail.com>
2023-02-19 16:06:07 +0100

    update day 31

--- Day_31_Capstone/day-31-Flash-card.py
+++ Day_31_Capstone/day-31-Flash-card.py
@@ -3,48 +3,75 @@ from tkinter import *
 import pandas as pd
 import random
 
+import pandas.errors
+
 # Global Variables
 CARD_FRONT_WIDTH = 800
 CARD_FRONT_HEIGHT = 526
 FONT_WORD = ("Ariel", 60, "bold")
 FONT_TITLE = ("Ariel", 40, "italic")
 WINDOW_COLOR_BG = "#B1DDC6"
+
 COUNT = 3
 TIMER = None
 DATA_FRAME = None
 WORD_DICO = {}
 
 try:
-    DATA_FRAME = pd.read_csv("./Dico/wordsToLean.csv")
+    DATA_FRAME = pd.read_csv("./Dico/wordsToLearn.csv")
 except FileNotFoundError:
     print("CSV not found")
     DATA_FRAME = pd.read_csv("./Dico/Frequency.csv")
+except pandas.errors.EmptyDataError:
+    DATA_FRAME = pd.read_csv("./Dico/Frequency.csv")
 finally:
-    toLearn = DATA_FRAME.to_dict(orient="records")
+    TO_LEARN = DATA_FRAME.to_dict(orient="records")
+
+
+# ---------------------------- UTILITY FUNCTION ------------------------------- #
+
+def error():
+    global TO_LEARN, DATA_FRAME
+    DATA_FRAME = pd.read_csv("./Dico/Frequency.csv")
+    TO_LEARN = DATA_FRAME.to_dict(orient="records")
 
 
 # ---------------------------- RIGHT BUTTON ------------------------------- #
 
 def correctAnswer():
-    toLearn.remove(WORD_DICO)
-    print(len(toLearn))
-    data = pd.DataFrame(toLearn)
-    data.to_csv("./Dico/wordsToLean.csv", index=False)
+    global TO_LEARN
+    try:
+        TO_LEARN.remove(WORD_DICO)
+    except ValueError:
+        error()
+    finally:
+        data = pd.DataFrame(TO_LEARN)
+        data.to_csv("./Dico/wordsToLearn.csv", index=False)
+        timerSwitch()
+
+
+# ---------------------------- WRONG BUTTON ------------------------------- #
+
+def wrongButton():
+    data = pd.DataFrame(TO_LEARN)
+    data.to_csv("./Dico/wordsToLearn.csv", index=False)
     timerSwitch()
 
 
 # ---------------------------- TIMER ------------------------------- #
 def timerSwitch():
-    global TIMER, DATA_FRAME, WORD_DICO
-    if len(DATA_FRAME) == 0:
-        DATA_FRAME = pd.read_csv("./Dico/Frequency.csv")
+    global TIMER, WORD_DICO, TO_LEARN
     if TIMER is not None:
         window.after_cancel(TIMER)
-    WORD_DICO = random.choice(toLearn)
-    canvas.itemconfig(titleCanvas, text="English")
-    canvas.itemconfig(wordCanvas, text=WORD_DICO['English'])
-    canvas.itemconfig(imageCanvas, image=cardFrontImage)
-    englishWord(COUNT)
+    try:
+        WORD_DICO = random.choice(TO_LEARN)
+    except IndexError:
+        error()
+    finally:
+        canvas.itemconfig(titleCanvas, text="English")
+        canvas.itemconfig(wordCanvas, text=WORD_DICO['English'])
+        canvas.itemconfig(imageCanvas, image=cardFrontImage)
+        englishWord(COUNT)
 
 
 # ---------------------------- ENGLISH WORD ------------------------------- #
@@ -81,7 +108,7 @@ titleCanvas = canvas.create_text(400, 150, text="English", font=FONT_TITLE)
 canvas.grid(column=1, row=0, columnspan=2)
 
 # Button
-wrongButton = Button(image=wrongButtonImage, highlightthickness=0, bg=WINDOW_COLOR_BG, command=timerSwitch)
+wrongButton = Button(image=wrongButtonImage, highlightthickness=0, bg=WINDOW_COLOR_BG, command=wrongButton)
 wrongButton.grid(column=0, row=1, columnspan=2)
 rightButton = Button(image=rightButtonImage, highlightthickness=0, bg=WINDOW_COLOR_BG, command=correctAnswer)
 rightButton.grid(column=2, row=1, columnspan=2)
