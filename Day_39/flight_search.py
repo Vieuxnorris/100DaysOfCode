1309014a4ce100c8b0dd8719b60b68a94aaaae4e
unknown <Vieuxnorris@gmail.com>
2023-02-26 20:30:16 +0100

    add Day 39

---
+++ Day_39/flight_search.py
@@ -0,0 +1,59 @@
+import os
+import requests
+import datetime as dt
+from flight_data import FlightData
+
+URL_FLIGHT = "https://api.tequila.kiwi.com/locations/query"
+URL_FLIGHT_SEARCH = "https://api.tequila.kiwi.com/v2/search"
+
+
+class FlightSearch:
+    # This class is responsible for talking to the Flight Search API.
+    def get_destination_code(self, city_name):
+        location_endpoint = URL_FLIGHT
+        headers = {"apikey": os.getenv("API_FLIGHT")}
+        query = {"term": city_name, "location_types": "city"}
+        response = requests.get(url=location_endpoint, headers=headers, params=query)
+        results = response.json()["locations"]
+        code = results[0]["code"]
+        return code
+
+    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
+        headers = {"apikey": os.getenv("API_FLIGHT")}
+        query = {
+            "fly_from": origin_city_code,
+            "fly_to": destination_city_code,
+            "date_from": from_time.strftime("%d/%m/%Y"),
+            "date_to": to_time.strftime("%d/%m/%Y"),
+            "nights_in_dst_from": 7,
+            "nights_in_dst_to": 28,
+            "flight_type": "round",
+            "one_for_city": 1,
+            "max_stopovers": 0,
+            "curr": "EUR"
+        }
+
+        response = requests.get(
+            url=URL_FLIGHT_SEARCH,
+            headers=headers,
+            params=query,
+        )
+
+        try:
+            data = response.json()["data"][0]
+        except IndexError:
+            print(f"No flights found for {destination_city_code}.")
+            return None
+
+        flight_data = FlightData(
+            price=data["price"],
+            origin_city=data["route"][0]["cityFrom"],
+            origin_airport=data["route"][0]["flyFrom"],
+            destination_city=data["route"][0]["cityTo"],
+            destination_airport=data["route"][0]["flyTo"],
+            out_date=data["route"][0]["local_departure"].split("T")[0],
+            return_date=data["route"][1]["local_departure"].split("T")[0]
+        )
+        print(f"{flight_data.destination_city}: €{flight_data.price}")
+        return flight_data
+
---
+++ Day_39/notification_manager.py
@@ -0,0 +1,19 @@
+import os
+
+from twilio.rest import Client
+
+
+class NotificationManager:
+    # This class is responsible for sending notifications with the deal flight details.
+    def sendSMS(self, dataGoogleSheet, flightInfos):
+        if dataGoogleSheet['lowestPrice'] > flightInfos.price:
+            account_sid = "ACc2e766270f4c28a1294bf59a4a837eaa"
+            auth_token = os.environ["TWILIO_TOKEN"]
+            client = Client(account_sid, auth_token)
+            message = client.messages.create(
+                body=f"Low price alert! Only €{flightInfos.price} to fly from {flightInfos.origin_city}"
+                     f"to {flightInfos.destination_city}, from {flightInfos.out_date} to {flightInfos.return_date}.",
+                from_="+12706336456",
+                to="+32474361140"
+            )
+            print(message.sid)
