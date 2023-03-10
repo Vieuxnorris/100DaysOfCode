40bf9e374efbc7306664a02151fcbb3d11b1eb70
Vieuxnorris <Vieuxnorris@gmail.com>
2023-03-10 15:43:56 +0100

    update return value and format

--- Day_51/internetSpeedTwitterBot.py
+++ Day_51/internetSpeedTwitterBot.py
@@ -1,13 +1,19 @@
 from Utils import *
 from selenium import webdriver
+
+
 class InternetSpeedTwitterBot:
     def __init__(self):
         self.driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH)
         self.up = 0
         self.down = 0
 
-    def getInternetSpeed(self) -> tuple:
-        return (self.down, self.up)
+    def getInternetSpeed(self) -> dict:
+        returnSpeed = {
+            "down: ": self.down,
+            "up: ": self.up,
+        }
+        return returnSpeed
 
     def tweetAtProvider(self) -> str:
         return f"#VOO my internet speed Down -> {self.down} and my Up -> {self.up}"
