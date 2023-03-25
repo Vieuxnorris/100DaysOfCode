0c448b6f5edc122d20afc4f907f5f6357b68d290
Vieuxnorris <Vieuxnorris@gmail.com>
2023-03-25 18:52:17 +0100

    update database

--- Day_65/Project/main.py
+++ Day_65/Project/main.py
@@ -21,7 +21,7 @@ Bootstrap(app)
 class bookDB(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), unique=True, nullable=False)
-    author = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
     rating = db.Column(db.Float, nullable=False)
 
 
