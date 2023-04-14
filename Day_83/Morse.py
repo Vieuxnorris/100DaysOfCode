52f215ddda892dc95f4fbf7668a4a32438a559ff
Vieuxnorris <Vieuxnorris@gmail.com>
2023-04-14 21:43:42 +0200

    update

--- Day_83/Morse.py
+++ Day_83/Morse.py
@@ -1,18 +1,18 @@
 import pandas as pd
 
+
 def openCsv(file):
     data = pd.read_csv(file)
     dictData = {value.letter: value.morse for (index, value) in data.iterrows()}
     return dictData
 
 
-
 if __name__ == '__main__':
     morseData = openCsv('morse.csv')
     phrase = input("Enter your message in Morse:")
-
-    output = [morseData.get(letter.upper()) for letter in phrase]
-
-    print(f"{' '.join(output)}")
-
+    try:
+        output = [morseData.get(letter.upper()) for letter in phrase]
+        print(f"{' '.join(output)}")
+    except TypeError:
+        print('Error Code Morse')
 
